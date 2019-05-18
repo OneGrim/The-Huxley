3ad56ddf9934ebd7b0c26ebcb1e657a259c7bfc8
OneGrim <50768761+OneGrim@users.noreply.github.com>
2019-05-18 08:00:34 -0300

    Delete Soma de Fatoriais.py

--- Roteiro 7/Soma de Fatoriais.py
+++
@@ -1,7 +0,0 @@
-def fact(number):
-    return number if number == 1 else number * fact(number - 1)
-Sum = 0
-for i in range(5):
-    Number = int(input())
-    if Number > 0 and Number % 3 == 0: Sum += fact(Number)
-print(Sum)
